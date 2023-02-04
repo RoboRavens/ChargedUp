797811562106f20200e8acbb7dd313f9fb3a4565
Tair K <karanfilitair@outlook.com>
2023-02-04 12:37:59 -0500

    Limelight Trajectory Generation

---
+++ src/main/java/frc/robot/LimelightTrajectorySubsystem.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class LimelightTrajectorySubsystem {
+
+        Field2d m_field = new Field2d();
+         
+
+        public void generateTrajectory() {
+
+              
+                var robotStartingPosition = new Pose2d(Units.feetToMeters(Robot.LIMELIGHT_SUBSYSTEM.getBotpose()),
+                                Units.feetToMeters(Robot.LIMELIGHT_SUBSYSTEM.getBotpose()),
+                                Rotation2d.fromDegrees(Robot.LIMELIGHT_SUBSYSTEM.getBotpose()));
+                var coneTargetLocation = new Pose2d(Units.feetToMeters(5.56), Units.feetToMeters(5.14),
+                                Rotation2d.fromDegrees(64.80));
+
+                TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(12), Units.feetToMeters(12));
+                config.setReversed(true);
+
+                var trajectory = TrajectoryGenerator.generateTrajectory(
+                                robotStartingPosition,
+                                null, coneTargetLocation,
+                                config);
+
+                        
+                }
+}
--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,50 +1,53 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class LimelightSubsystem {
-    
-   LimelightSubsystem LIMELIGHT_SUBSYSTEM = new LimelightSubsystem();
 
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-    NetworkTableEntry ts = table.getEntry("ts");
-    NetworkTableEntry tv = table.getEntry("tv");
-    int camMode = 0;
+  LimelightSubsystem LIMELIGHT_SUBSYSTEM = new LimelightSubsystem();
 
-    
-    
-    // how many degrees back is your limelight rotated from perfectly vertical?
-   double limelightMountAngleDegrees = 25.0;
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+  NetworkTableEntry ts = table.getEntry("ts");
+  NetworkTableEntry tv = table.getEntry("tv");
+  NetworkTableEntry botpose = table.getEntry("botpose");
+  int camMode = 0;
 
-   // distance from the center of the Limelight lens to the floor
-   double limelightLensHeightInches = 20.0;
+  // how many degrees back is your limelight rotated from perfectly vertical?
+  double limelightMountAngleDegrees = 25.0;
 
-   // distance from the target to the floor
-   double goalHeightInches = 60.0;
-    
-   /*  public double limelightPipelineConfiguration() {
+  // distance from the center of the Limelight lens to the floor
+  double limelightLensHeightInches = 20.0;
 
-      if (PIPELINEBUTTON0ISPRESSED) {
-        int pipeline = 0;
-     }
+  // distance from the target to the floor
+  double goalHeightInches = 60.0;
 
-      if (PIPELINEBUTTON1ISPRESSED) {
-        int pipeline = 1;
-     }
+  /*
+   * public double limelightPipelineConfiguration() {
+   * 
+   * if (PIPELINEBUTTON0ISPRESSED) {
+   * int pipeline = 0;
+   * }
+   * 
+   * if (PIPELINEBUTTON1ISPRESSED) {
+   * int pipeline = 1;
+   * }
+   * 
+   * if (pipeline = 1 & LEFTTRIGGERISPULLED) {
+   * int ledMode = 3;
+   * }
+   * }
+   */
 
-      if (pipeline = 1 & LEFTTRIGGERISPULLED) {
-        int ledMode = 3;
-     }
-   }
-    */
-   
-    public double getTx() {
+  public double getTx() {
     return tx.getDouble(0.0);
   }
 
@@ -56,23 +59,22 @@ public class LimelightSubsystem {
     return ty.getDouble(0.0);
   }
 
+  public Double getBotpose() {
+    return botpose.getDouble(0.0);
+   
+  }
+
   public double getDistance() {
     double targetOffsetAngle_Vertical = getTy();
 
-   double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-   double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
 
-   //calculate distance
-   double distanceFromLimelightToTargetInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
-   SmartDashboard.putNumber("DISTANCE FROM TARGET", distanceFromLimelightToTargetInches);
-   return distanceFromLimelightToTargetInches;
-}
-  
-
-
+    // calculate distance
+    double distanceFromLimelightToTargetInches = (goalHeightInches - limelightLensHeightInches)
+        / Math.tan(angleToGoalRadians);
+    SmartDashboard.putNumber("DISTANCE FROM TARGET", distanceFromLimelightToTargetInches);
+    return distanceFromLimelightToTargetInches;
+  }
 
 }
-
-
-
-
