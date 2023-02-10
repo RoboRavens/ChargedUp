2ffac1e219efa2cb885e7291eca9a9c1ce357dd9
milligansarah <98560997+milligansarah@users.noreply.github.com>
2023-02-09 19:19:31 -0500

    Added two piece auto path

--- src/main/java/frc/robot/commands/auto/TestAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/TestAutoCommand.java
@@ -1,23 +1,16 @@
 package frc.robot.commands.auto;
 
-import java.util.List;
-
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
 import frc.util.AutoMode;
 
 public class TestAutoCommand {
     public static AutoMode getAutoMode() {
-        Trajectory examplePath = PathPlanner.loadPath("New Path", new PathConstraints(1.5, 0.6));
+        Trajectory examplePath = PathPlanner.loadPath("Two Piece Auto", new PathConstraints(1, 0.4));
         // Trajectory exampleTrajectory =
         // TrajectoryGenerator.generateTrajectory(
         //     // Start at the origin facing the +X direction
