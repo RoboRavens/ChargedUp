7b4c633902926a67670ce0af6ffe31b52362f71b
Tair K <karanfilitair@outlook.com>
2023-03-09 16:17:51 -0500

    Offset

--- src/main/java/frc/robot/subsystems/LimelightTrajectorySubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightTrajectorySubsystem.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
 
 public class LimelightTrajectorySubsystem extends SubsystemBase {
@@ -45,32 +46,31 @@ public class LimelightTrajectorySubsystem extends SubsystemBase {
     public LimelightTrajectorySubsystem() {
 
         SmartDashboard.putData("trajectory", DASHBOARD_Field2d);
-        
 
     }
 
     public Pose2d selectEndpoint() {
         SendableChooser<Pose2d> poseChooser = new SendableChooser<>();
-        Pose2d row1BlueAlliance = new Pose2d(1.9, 0.46, Rotation2d.fromDegrees(0));
-        Pose2d row2BlueAlliance = new Pose2d(1.87, 1.09, Rotation2d.fromDegrees(0));
-        Pose2d row3BlueAlliance = new Pose2d(1.91, 1.62, Rotation2d.fromDegrees(0));
-        Pose2d row4BlueAlliance = new Pose2d(1.89, 2.19, Rotation2d.fromDegrees(0));
-        Pose2d row5BlueAlliance = new Pose2d(1.91, 2.72, Rotation2d.fromDegrees(0));
-        Pose2d row6BlueAlliance = new Pose2d(1.91, 3.28, Rotation2d.fromDegrees(0));
-        Pose2d row7BlueAlliance = new Pose2d(1.91, 3.84, Rotation2d.fromDegrees(0));
-        Pose2d row8BlueAlliance = new Pose2d(1.91, 4.39, Rotation2d.fromDegrees(0));
-        Pose2d row9BlueAlliance = new Pose2d(1.91, 5.02, Rotation2d.fromDegrees(0));
-        Pose2d row1RedAlliance = new Pose2d(14.61, 5.02, Rotation2d.fromDegrees(0));
-        Pose2d row2RedAlliance = new Pose2d(14.61, 4.39, Rotation2d.fromDegrees(0));
-        Pose2d row3RedAlliance = new Pose2d(14.61, 3.85, Rotation2d.fromDegrees(0));
-        Pose2d row4RedAlliance = new Pose2d(14.61, 3.29, Rotation2d.fromDegrees(0));
-        Pose2d row5RedAlliance = new Pose2d(14.61, 2.73, Rotation2d.fromDegrees(0));
-        Pose2d row6RedAlliance = new Pose2d(14.61, 2.16, Rotation2d.fromDegrees(0));
-        Pose2d row7RedAlliance = new Pose2d(14.61, 1.62, Rotation2d.fromDegrees(0));
-        Pose2d row8RedAlliance = new Pose2d(14.61, 1.06, Rotation2d.fromDegrees(0));
-        Pose2d row9RedAlliance = new Pose2d(14.61, 0.5, Rotation2d.fromDegrees(0));
+        Pose2d row1BlueAlliance = new Pose2d(1.9 + Constants.ROBOT_SCORING_OFFSET, 0.46, Rotation2d.fromDegrees(0));
+        Pose2d row2BlueAlliance = new Pose2d(1.87 + Constants.ROBOT_SCORING_OFFSET, 1.09, Rotation2d.fromDegrees(0));
+        Pose2d row3BlueAlliance = new Pose2d(1.91 + Constants.ROBOT_SCORING_OFFSET, 1.62, Rotation2d.fromDegrees(0));
+        Pose2d row4BlueAlliance = new Pose2d(1.89 + Constants.ROBOT_SCORING_OFFSET, 2.19, Rotation2d.fromDegrees(0));
+        Pose2d row5BlueAlliance = new Pose2d(1.91 + Constants.ROBOT_SCORING_OFFSET, 2.72, Rotation2d.fromDegrees(0));
+        Pose2d row6BlueAlliance = new Pose2d(1.91 + Constants.ROBOT_SCORING_OFFSET, 3.28, Rotation2d.fromDegrees(0));
+        Pose2d row7BlueAlliance = new Pose2d(1.91 + Constants.ROBOT_SCORING_OFFSET, 3.84, Rotation2d.fromDegrees(0));
+        Pose2d row8BlueAlliance = new Pose2d(1.91 + Constants.ROBOT_SCORING_OFFSET, 4.39, Rotation2d.fromDegrees(0));
+        Pose2d row9BlueAlliance = new Pose2d(1.91 + Constants.ROBOT_SCORING_OFFSET, 5.02, Rotation2d.fromDegrees(0));
+        Pose2d row1RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 5.02, Rotation2d.fromDegrees(0));
+        Pose2d row2RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 4.39, Rotation2d.fromDegrees(0));
+        Pose2d row3RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 3.85, Rotation2d.fromDegrees(0));
+        Pose2d row4RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 3.29, Rotation2d.fromDegrees(0));
+        Pose2d row5RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 2.73, Rotation2d.fromDegrees(0));
+        Pose2d row6RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 2.16, Rotation2d.fromDegrees(0));
+        Pose2d row7RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 1.62, Rotation2d.fromDegrees(0));
+        Pose2d row8RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 1.06, Rotation2d.fromDegrees(0));
+        Pose2d row9RedAlliance = new Pose2d(14.61 + Constants.ROBOT_SCORING_OFFSET, 0.5, Rotation2d.fromDegrees(0));
         poseChooser.setDefaultOption("Initial Pose", row1BlueAlliance);
-        poseChooser.addOption("ROW 1 CONE BLUE ALLIANCE", row1BlueAlliance );
+        poseChooser.addOption("ROW 1 CONE BLUE ALLIANCE", row1BlueAlliance);
         poseChooser.addOption("ROW 2 CUBE BLUE ALLIANCE", row2BlueAlliance);
         poseChooser.addOption("ROW 3 CONE BLUE ALLIANCE", row3BlueAlliance);
         poseChooser.addOption("ROW 4 CONE BLUE ALLIANCE", row4BlueAlliance);
@@ -92,7 +92,7 @@ public class LimelightTrajectorySubsystem extends SubsystemBase {
         Pose2d selectedPose = poseChooser.getSelected();
         return selectedPose;
     }
-    
+
     public void periodic() {
 
         goToScoringPosition();
