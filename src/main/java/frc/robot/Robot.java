d770e5f9dc910fd027ad1084dae83b5b766e3787
milligansarah <98560997+milligansarah@users.noreply.github.com>
2023-03-11 12:16:11 -0500

    Committing stashed changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.controls.Gamepad;
+import frc.robot.commands.auto.FourPieceAutoCommand;
 import frc.robot.commands.auto.PreloadAndBalanceAutoCommand;
 import frc.robot.commands.auto.TwoPieceAndBalanceAutoCommand;
 import frc.robot.commands.auto.TwoPieceAutoCommand;
@@ -73,7 +74,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = TwoPieceAndBalanceAutoCommand.getAutoMode().getAutoCommand();
+    m_autonomousCommand = FourPieceAutoCommand.getAutoMode().getAutoCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
---
+++ src/main/java/frc/robot/commands/auto/FourPieceAutoCommand.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.auto;
+
+import java.util.HashMap;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.FollowPathWithEvents;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Robot;
+import frc.robot.commands.TempAutoIntakeCommand;
+import frc.util.AutoMode;
+
+public class FourPieceAutoCommand {
+    public static AutoMode getAutoMode() {
+        // At this velocity and acceleration, the robot is able to score all four pieces in 29.81 seconds according to PathFinder
+        PathPlannerTrajectory examplePath = PathPlanner.loadPath("Four Piece Auto", new PathConstraints(2, 1));
+
+        HashMap<String, Command> eventMap = new HashMap<>();
+        eventMap.put("Retrieve Piece 2", new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()));
+        eventMap.put("Score Piece 2", new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()));
+        eventMap.put("Retrieve Piece 3", new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()));
+        eventMap.put("Score Piece 3", new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()));
+        eventMap.put("Retrieve Piece 4", new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()));        
+
+        FollowPathWithEvents examplePathWithEvents = new FollowPathWithEvents(
+            Robot.DRIVE_TRAIN_SUBSYSTEM.CreateFollowTrajectoryCommandSwerveOptimized(examplePath), 
+            examplePath.getMarkers(), 
+            eventMap);
+
+        Command testCommand = Robot.DRIVE_TRAIN_SUBSYSTEM.CreateSetOdometryToTrajectoryInitialPositionCommand(examplePath)
+        .andThen(new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()))
+        .andThen(examplePathWithEvents)
+        .andThen(new WaitCommand(1).deadlineWith(new TempAutoIntakeCommand()));
+
+        return new AutoMode("Test Auto", testCommand);
+    }
+}
