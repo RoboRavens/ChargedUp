a52558693b83245a9f9d8551e2da9e632e4d905d
milligansarah <98560997+milligansarah@users.noreply.github.com>
2023-03-11 09:23:49 -0500

    Completed drive two inches command

--- src/main/java/frc/robot/commands/drivetrain/DriveTwoInchesCommand.java
+++ src/main/java/frc/robot/commands/drivetrain/DriveTwoInchesCommand.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.util.StateManagement.OverallState;
 
 public class DriveTwoInchesCommand extends CommandBase {
     private Pose2d _targetPose;
@@ -90,8 +91,14 @@ public class DriveTwoInchesCommand extends CommandBase {
             }
         }
         if (Robot.DRIVE_TRAIN_SUBSYSTEM.powerIsCut()) {
-            xSpeed *= 2;
-            ySpeed *= 2;
+            if (Robot.overallState == OverallState.ENDGAME) {
+                xSpeed *= 4;
+                ySpeed *= 4;
+            }
+            else {
+                xSpeed *= 2;
+                ySpeed *= 2;
+            }
         }
         SmartDashboard.putNumber("xOffset", xOffsetFromTarget);
         SmartDashboard.putNumber("yOffset", yOffsetFromTarget);
