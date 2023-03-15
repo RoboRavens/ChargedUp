fce27e7f203db5d3780308e229c0cf3f1972d472
milligansarah <98560997+milligansarah@users.noreply.github.com>
2023-03-14 20:39:09 -0400

    2023 Drivetrain Steer Module Offsets

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,25 +10,29 @@ public class RobotMap {
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 24;
 
     // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(73.301); // practice
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(130); // competition
+    // public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(130); // competition
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(83); // 2023 competition
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 22;
     // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(320.625); // practice
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(79); // competition
+    // public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(79); // competition
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(42); // 2023 competition
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 21;
     // public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(191.338); // practice
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(240); // competition
+    // public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(240); // competition
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(71); // 2023 competition
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 4;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 23;
     // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(201.885); // practice
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(288); // competition
+    // public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(288); // competition
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(105); // 2023 competition
     public static final int PIECE_SENSOR = 9;
 
     // ARM
